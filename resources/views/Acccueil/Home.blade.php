ab8eb6760dbada9056a2af00faea2111d4d26878
jadaniayoub <jadaniayoub@gmail.com>
2024-03-30 17:46:31 +0000

    add for laravel

---
+++ resources/views/Acccueil/Home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <h2>Welcome to my website!</h2>
+        <p>This is the home page content.</p>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html> 
+<html lang="fr"> 
+<head> 
+<title>Liste des employés : insérer, Lire, modifier et 
+supprimer</title> 
+<meta charset="utf-8"> 
+<meta name="viewport" content="width=device-width, initial
+scale=1"> 
+</head> 
+
+<body> 
+<div>
+    <h1>navvvv</h1>
+</div>
+
+
+
+<div> 
+@yield('content') 
+</div> 
+
+
+
+<div>
+   <h1>footer</h1>
+</div>
+
+
+
+</body> 
+</html> 
