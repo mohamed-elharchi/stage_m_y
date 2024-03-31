0ca8ee375989fcfb06a169eedcd6428864caaf89
jadaniayoub <jadaniayoub@gmail.com>
2024-03-31 00:31:34 +0000

    add commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html> 
 <html lang="fr"> 
 <head> 
-<title>Liste des employés : insérer, Lire, modifier et 
-supprimer</title> 
+<title>lycce moulay ismail</title> 
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <meta charset="utf-8"> 
 <meta name="viewport" content="width=device-width, initial
 scale=1"> 
 </head> 
 
 <body> 
-<div>
+<div class="nav">
     <h1>navvvv</h1>
 </div>
 
 
 
-<div> 
+<div class="ja"> 
 @yield('content') 
 </div> 
 
