6c512d606bb344d22a1a067e026af6a9a8e19386
jadaniayoub <jadaniayoub@gmail.com>
2024-03-31 19:12:09 +0000

    day 1

--- resources/views/Acccueil/Home.blade.php
+++ resources/views/Acccueil/Home.blade.php
@@ -1,8 +1,42 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+}
 
-@section('content')
-    <div>
-        <h2>Welcome to my website!</h2>
-        <p>This is the home page content.</p>
+    </style>
+</head>
+<body>
+ @extends('layouts.app')   
+
+ @section('content')
+ <!--  Home -->
+
+ <section id="Home">
+    <div class="text">
+    <h2>lycée Moulay Ismail     </h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi vel quos nisi harum minus neque repellendus? At, adipisci earum animi cumque tempora quae harum, in natus quod maxime, accusantium asperiores.</p>
+    
     </div>
+
+    <button class="btn-left" id="prevBtn">&lt;</button>
+    <button class="btn-right" id="nextBtn">&gt;</button>
+</section>
+
+
+
 @endsection
+
+
+
+</body>
+</html>
+
+
+
+
+
