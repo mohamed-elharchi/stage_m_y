c73577cddf360989d145165f150eb10d8e9e8043
jadaniayoub <jadaniayoub@gmail.com>
2024-04-05 14:20:35 +0000

    oke

--- resources/views/Acccueil/Home.blade.php
+++ resources/views/Acccueil/Home.blade.php
@@ -224,8 +224,58 @@
     <!--end  comantairs-->
 
     <!-- contact -->
+   <section id="contact">
+    <h1>Envoie-nous un message</h1>
+    <p>Besoin d'aide ? Laissez-nous un message ! Nous sommes là pour vous aider avec toutes vos questions et préoccupations.</p>
 
 
+
+    <div class="container">
+        <div class="content">
+          <div class="left-side">
+            <div class="address details">
+                <i class="ri-map-pin-line"></i>
+              <div class="topic">Adresse</div>
+              <div class="text-one">Driouch</div>
+              <div class="text-two">HAY AL AMAL 06</div>
+            </div>
+            <div class="phone details">
+                <i class="ri-phone-line"></i>
+              <div class="topic">Téléphone</div>
+              <div class="text-one">+212 70652357</div>
+              <div class="text-two">+212 62OO21O1</div>
+            </div>
+            <div class="email details">
+                <i class="ri-mail-line"></i>
+              <div class="topic">Email</div>
+              <div class="text-one">moulay_ismail@gmail.com</div>
+              <div class="text-two">info_ms@gmail.com</div>
+            </div>
+          </div>
+          <div class="right-side">
+            <div class="topic-text">Contact</div>
+            <p>Si vous avez un travail de ma part ou tout type de requêtes liées à mon tutoriel, vous pouvez m'envoyer un message à partir d'ici. C'est avec plaisir que je vous aide.</p>
+          <form action="#">
+            <div class="input-box">
+              <input type="text" placeholder="Entrez votre nom">
+            </div>
+            <div class="input-box">
+              <input type="text" placeholder="Entrer votre Email">
+            </div>
+            <div class="input-box message-box">
+                <input type="texteria" placeholder=" Entrez votre message">
+            </div>
+            <div class="button">
+              <input type="button" value="Envoyer" >
+            </div>
+          </form>
+        </div>
+        </div>
+      </div>
+
+    
+   </section>
+   
      <!--  end contact -->
 
 
