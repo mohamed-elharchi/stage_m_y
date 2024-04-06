0368d4539ece2ba6631fd8f6964cd49cfaa5f229
zitouni mohamed <zitounirabab85@gmail.com>
2024-04-06 01:35:01 +0000

    gf

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\UtilisationDuTempsController;
 
 
 Route::resource('news', NewsController::class);
-Route::resource('utilisations', UtilisationDuTempsController::class);:
+Route::resource('utilisations', UtilisationDuTempsController::class);
 
 
 
