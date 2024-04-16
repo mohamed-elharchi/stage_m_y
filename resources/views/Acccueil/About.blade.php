9b114c30ee8a60577fd96c6a0751333d2eddffe5
jadaniayoub <jadaniayoub@gmail.com>
2024-04-16 17:18:51 +0100

    ok

--- resources/views/Acccueil/About.blade.php
+++ resources/views/Acccueil/About.blade.php
@@ -14,9 +14,33 @@
         <img src="{{ asset('images/Purple & Yellow Minimal School Admission Facebook Post (1).jpg') }}" alt="Description de l'image">
        </div>
 </section>
+<section class="slide">
+
+    <div class="wrapper">
+        <i id="left" class="ri-arrow-left-s-line"></i>
+        <div class="carousel">
+          <img src="images/Purple & Yellow Minimal School Admission Facebook Post (1).jpg" alt="img" draggable="false">
+          <img src="images/Purple & Yellow Minimal School Admission Facebook Post (1).jpg" alt="img" draggable="false">
+          <img src="images/Purple & Yellow Minimal School Admission Facebook Post (1).jpg" alt="img" draggable="false">
+          <img src="images/Purple & Yellow Minimal School Admission Facebook Post (1).jpg" alt="img" draggable="false">
+          <img src="images/Purple & Yellow Minimal School Admission Facebook Post (1).jpg" alt="img" draggable="false">
+          <img src="images/Purple & Yellow Minimal School Admission Facebook Post (1).jpg" alt="img" draggable="false">
+          <img src="images/Purple & Yellow Minimal School Admission Facebook Post (1).jpg" alt="img" draggable="false">
+          <img src="images/Purple & Yellow Minimal School Admission Facebook Post (1).jpg" alt="img" draggable="false">
+          <img src="images/Purple & Yellow Minimal School Admission Facebook Post (1).jpg" alt="img" draggable="false">
+        </div>
+        <i id="right" class="ri-arrow-right-s-line"></i>
+      </div>
+
+
+
+
+</section>
+   
 <section class="map_section">
     <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3269.4199612441193!2d-3.3921869753247855!3d34.97114406865483!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd7642c18db3ccb7%3A0xfea1c5ab108af126!2z2KvYp9mG2YjZitipINmF2YjZhNin2Yog2KfYs9mF2KfYudmK2YQ!5e0!3m2!1sar!2sma!4v1713117795992!5m2!1sar!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </section>
 
 @endsection
 <link rel="stylesheet" href="{{ asset('css/About.css') }}">
+<script src="js/About.js"></script>
