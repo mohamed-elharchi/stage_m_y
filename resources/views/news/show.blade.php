51e5a3d33a432a68e5be5ac2fb8e99f2c456b609
zitouni mohamed <zitounirabab85@gmail.com>
2024-04-30 16:16:55 +0100

    kn

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,200 +1,76 @@
 @extends('layouts.Acceuill')
 @section('content')
-<!DOCTYPE html>
-<html >
-<head>
-  <meta charset="UTF-8">
-  <title>Slider using property Clip</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
+<style>
+/* General styles */
 
-    <style>
-
-.a {
-	position: relative;
-	width: 100%;
-	height: 100vh;
-    margin-bottom: 50px;
-    margin-top: 80px;
-    overflow: auto;
-}
-.lefteee {
-	float: left;
-	height: 100%;
-	width: 60%;
-	padding: 3rem 3rem 3rem 5rem;
-	display: table;
-
-}
-.lefteee > div {
-	display: table-cell;
-	vertical-align: middle;
-}
-span {
-  color: #E8CA2B;
-  font-size: 14px;
-  font-weight: bold;
-  letter-spacing: 2px;
-  display: inline-block;
-  text-transform: uppercase;
-  font-family: sans-serif;
-  margin-bottom: 4rem;
-}
-.zzz {
-	font-size: 4rem;
-	margin-bottom: 3rem;
-}
-.zzz + .bb {
-  color: #949494;
-  font-size: 1.6rem;
-  margin-bottom: 4rem;
-}
-.bb  {
-  font-size: 2.6rem;
-  color: #000;
-
-
-
-}
-.sliderrr {
-	float: right;
-	position: relative;
-	width: 40%;
-	height: 100%;
-    margin-top: 48px;
-
-}
-.sliderrr li {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	background-size: cover;
-	background-repeat: no-repeat;
-	background-position: 50% 50%;
-	-webkit-transition: clip .7s ease-in-out, z-index 0s .7s;
-	transition: clip .7s ease-in-out, z-index 0s .7s;
-	clip: rect(0, 100vw, 100vh, 100vw);
-	display: table;
-}
-.centerrr-y {
-	display: table-cell;
-	vertical-align: middle;
-	text-align: center;
-	color: #fff;
-}
-h3 {
-	font-size: 5rem;
-	font-style: italic;
-}
-h3   {
-	font-size: 1.6rem;
-	display: inline-block;
-	color: #fff;
-	margin-top: 2rem;
-}
-h3, h3 +  {
-	opacity: 0;
-	-webkit-transition: opacity .7s 0s, -webkit-transform .5s .2s;
-	transition: opacity .7s 0s, -webkit-transform .5s .2s;
-	transition: opacity .7s 0s, transform .5s .2s;
-	transition: opacity .7s 0s, transform .5s .2s, -webkit-transform .5s .2s;
-	-webkit-transform: translate3d(0, 50%, 0);
-	        transform: translate3d(0, 50%, 0);
-}
-li.current h3, li.current h3 +  {
-	opacity: 1;
-	-webkit-transition-delay: 1s;
-	        transition-delay: 1s;
-	-webkit-transform: translate3d(0, 0, 0);
-	        transform: translate3d(0, 0, 0);
-}
-li.current {
-	z-index: 1;
-	clip: rect(0, 100vw, 100vh, 0);
-}
-li.prev {
-	clip: rect(0, 0, 100vh, 0);
-}
-.sliderrr navv {
-	position: absolute;
-	bottom: 5%;
-	left: 0;
-	right: 0;
-	text-align: center;
-	z-index: 10;
+/* Flex container */
+.dd {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 50px; /* Allow flex items to wrap to next line */
 }
 
-.current_dot {
-	-webkit-transform: scale(1.4);
-	        transform: scale(1.4);
-}
-@media screen and (max-width: 700px) {
-	.lefteee {
-		width: 100%;
-
-	}
-	.sliderrr {
-		width: 100%;
-
-
-	}
-    .zzz{
-        font-size: 25px;
-    }
-.zzz + .bb {
-  color: #949494;
-  font-size: 20px;
-  margin-bottom: 4rem;
+/* Left side (image) */
+.f1 {
+  width: 100%; /* Take full width on small screens */
+  padding: 20px;
 }
 
+.f1 img {
+  width: 100%;
+  height: auto;
+  display: block;
 }
 
+/* Right side (text) */
+.f2 {
+  width: 100%; /* Take full width on small screens */
+  padding: 20px;
+}
 
-    </style>
+.f2 p {
+  font-size: 1.2rem;
+  line-height: 1.6;
+}
 
+/* Sticky elements */
+.sticky {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  width: 100%; /* Take full width on small screens */
+}
 
-</head>
+/* Styles for specific screen sizes */
+@media only screen and (min-width: 600px) {
+  /* Adjustments for screens wider than 600px */
+  .f1 {
+    width: 30%;
+  }
 
-<body>
+  .f2 {
+    width: 70%;
+  }
+}
+</style>
+
 @include('Acccueil.zi')
-<div class="a">
-      <div class="lefteee">
-        <div class="zzzz">
 
-          <h1 class="zzz"> {{ $news->title }}</h1>
-          <span>{{ \Carbon\Carbon::parse($news->created_at)->format('d M Y') }}</span>
-          <p class="bb">{{ $news->paragraph }} </p>
-        </div>
-      </div>
-
-      <div class="sliderrr">
-        <ul>
-          <li style="background-image:url({{ asset('imagess/' . $news->image) }}">
-          </li>
-
-        </ul>
-      </div>
+<div class="dd">
+  <div class="f1">
+    <img class="sticky" src="{{ asset('imagess/' . $news->image) }}" alt="Avatar">
+    <h2 class="sticky">{{ $news->title }}</h2>
+    <span>{{ \Carbon\Carbon::parse($news->created_at)->format('d M Y') }}</span>
+  </div>
+  <div class="f2">
+    <p>
+      {{ $news->paragraph }}
+    </p>
+  </div>
 </div>
 
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
 
 
-    <script>
-      $(function() {
-	var slider = $(".sliderrr"),
-		slides = slider.find('li'),
-		nav = slider.find('navv');
-
-	slides.eq(0).addClass('current');
-
-	nav.children('').eq(0).addClass('current_dot');
-
-	});
-    </script>
-
-</body>
-</html>
 @endsection
