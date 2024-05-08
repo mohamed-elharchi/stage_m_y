a594b396688089dc8ef9453a535ab044a302f881
jadaniayoub <jadaniayoub@gmail.com>
2024-05-08 20:32:25 +0100

    ok

--- resources/views/layouts/Acceuill.blade.php
+++ resources/views/layouts/Acceuill.blade.php
@@ -6,10 +6,11 @@
     <link href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <script src="js/my.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
     <!-- image icon page ++++++++++  -->
-    <link rel="icon" href="{{ asset('images/moulay ismai (2).png') }}" type="image/x-icon"/>
+    <link rel="icon" href="{{ asset('images/moulay ismai (2).png') }}" type="image/x-icon" />
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -81,7 +82,7 @@
         <div class="footer-right">
             <p class="footer-company-about">
                 <span><i class="ri-attachment-2"></i> Mr le Directeur :</span>
-                <strong>Lycée Moulay Ismail</strong>  Bienvenue à notre lycée ! Nous sommes ravis de vous accueillir dans
+                <strong>Lycée Moulay Ismail</strong> Bienvenue à notre lycée ! Nous sommes ravis de vous accueillir dans
                 notre
                 communauté éducative dynamique où chaque élève est encouragé à exceller et à s'épanouir. Ensemble,
                 explorons
@@ -94,7 +95,7 @@
             </div>
         </div>
 
-      
+
         <div class="footer-left">
             <img src="{{ asset('images/mssmai.png') }}" alt="Example" class="logo2">
 
@@ -102,14 +103,14 @@
                 <a href="{{ route('accueil') }}">Accueil</a>
                 <br>
                 <a href="{{ route('About') }}">À propos</a>
-               <br>
+                <br>
                 <a href="{{ route('Calendriers') }}">Calendriers</a>
             </p>
 
             <p class="footer-company-name">Copyright © 2024 (JHBZ) <strong>Lycée Moulay Ismail </strong> Tous Droits
                 Réservés</p>
         </div>
-        
+
     </footer>
 
     <!--  end footer-->
@@ -120,51 +121,116 @@
     <a href="#" class="scrollup" id="scroll-up"> <i class="ri-arrow-up-s-line"></i> </a>
     <!-- togl -->
     <div class="discussion">
-        <div class="part1">
-            <h2>Ajouter Vos Témoignages</h2>
+        <div class="part11">
+            <div class="ppa">
+
+            </div>
+            <h3>:تتبع الطلب</h3>
+          <p>ادخل رقمك الهاتفي لتتابع حالة طلبك</p>
+            <div class="ppb">
+               <form>
+                <label>Numéro de téléphone:</label>
+                <input type="text" placeholder="(06 0000 0000)">
+                <button type="submit" class="bouton-principal" ><i class="ri-send-plane-fill"></i></button>
+               </form>
+               <div><p style="color: red">Lorem ipsum dolor sit pariatur.!!</p></div>
+
+             
+            </div>
+
+        </div>
+        <div class="part22">
+
+            <div class="part33">
+                <div class="pp3t">
+                    <h3>:طلب الشهادة المدرسية</h3>
+                    <p style="font-size: 0.8rem">اختر وضعيتك الحالية , هل انت منقطع عن الدراسه او حاصل على شهاده البكالورية,  او تدرس في المدرسه الثانوية </p>
+                </div>
+                <div class="pp3i">
+                    <div class="groupg">
+                        <input type="radio" name="gender" value="homme" id="hommeRadio" onclick="showSection('homme')" checked="checked">
+
+                        <label for="hommeRadio">ادرس في امدرسة الثانوية</label>
+                    </div>
+                    <div class="groupg">
+                        <input type="radio" name="gender" value="femme" id="femmeRadio" onclick="showSection('femme')">
+                        <label for="femmeRadio">منقطع عن الدراسة او حاصل على شهادة البكالورية</label>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+
+            <div. class="part44">
+                <div id="hommeSection" class="section">
+                    <div class="formulaire">
+
+                        <form>
+                            <div class="groupe">
+                                <label>Le nom complet:الاسم الكامل</label>
+                                <input type="text" placeholder="الاسم الكامل">
+                            </div>
+
+                            <div class="groupe">
+                                <label> Date de naissance:تاريخ الازدياد</label>
+                                <input type="date">
+                            </div>
+
+                            <div class="groupe">
+                                <label>Numéro de Massar:رقم مسار</label>
+                                <input type="text" placeholder="(H00000000000)">
+                            </div>
+
+                            <div class="groupe">
+                                <label>Numéro de téléphone:رقم الهاتف</label>
+                                <input type="text" placeholder="(06 0000 0000)">
+                            </div>
+                            <div>
+                                <button type="submit" class="bouton-principal" >Envoyer la demande</button>
+                                <label>:ارسال الطلب</label>
+                            </div>
+
+                        </form>
+                    </div>
+
+                </div>
+
+                <div id="femmeSection" class="section">
+
+                    <div class="formulaire">
+
+                        <form>
+                            <div class="groupe">
+                                <label>Le nom complet:الاسم الكامل</label>
+                                <input type="text" placeholder="الاسم الكامل">
+                            </div>
+                            <div class="groupe">
+                                <label> Dernier année scolaire :اخر سنه دراسيه</label>
+                                <input type="text" placeholder="0000/0000 ">
+                            </div>
+                            <div class="groupe">
+                                <label> Date de naissance:تاريخ الازدياد</label>
+                                <input type="date" placeholder="(0000-00-00)">
+                            </div>
+                            <div class="groupe">
+                                <label>Numéro de téléphone:رقم الهاتف</label>
+                                <input type="text" placeholder="(06 0000 0000)">
+                            </div>
+
+                            <button type="submit" class="bouton-principal">Envoyer la demande</button>
+                            <label>:ارسال الطلب</label>
+                        </form>
+                    </div>
+
+                </div>
         </div>
 
 
-        <div class="part2">
-
-            <p>
-                "Exprimez-vous ! Partagez vos expériences uniques avec nous.
-                Votre témoignage pourrait inspirer d'autres."
-            </p>
-        </div>
-
-        <dir class="part3">
-            <form class="formul-togl">
-
-                <div class="inps">
-                    <table>
-                        <tr>
-                            <td> <label for="a" class="i1"><i class="ri-id-card-line"></i></label></td>
-                            <td> <input id="a" type="text" name="nom" class="n1" required
-                                    placeholder="Entrez votre nom"></td>
-
-                        </tr>
-                        <tr>
-                            <td> <label for="b" class="i1"><i class="ri-calendar-2-line"></i></label></td>
-                            <td> <input id="b" type="text" name="time" class="n1" required
-                                    placeholder="Année scolaire"></td>
-
-                        </tr>
-
-                    </table>
-                </div>
 
 
-
-                <div class="msg">
-                    <input class="n2" type="text" placeholder="Entrez votre Témoignages ici...">
-                    <button type="submit" name="ajouter" class="n3"><i class="ri-send-plane-fill"></i></button>
-                </div>
-
-            </form>
-
-
-        </dir>
+    </div>
 
     </div>
 
